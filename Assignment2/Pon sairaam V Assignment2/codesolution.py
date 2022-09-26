3b45997e114c098d7561868da8eabb8199d9592c
kathirpf <113767190+kathirpf@users.noreply.github.com>
2022-09-26 23:06:36 +0530

    Update codesolution.py

--- Assignment2/Pon sairaam V Assignment2/codesolution.py
+++ Assignment2/Pon sairaam V Assignment2/codesolution.py
@@ -13,13 +13,14 @@ GPIO.setup(BUZZER,GPIO.OUT)
 
 
 while True:
-    temp = random.randint(0, 100)
-    humidity = random.randint(0, 100)
-    buzzstate = not buzzstate                
-	GPIO.output(BUZZER,buzzstate)
-    if(temp>=55):      
-	    print("temperature have crossed the threshold level on",dt.strftime("%d-%m-%y"),"at",dt.strftime("%H:%M"))
-    elif(humidity>=78):
-        print("humidity have crossed the threshold level on",dt.strftime("%d-%m-%y"),"at",dt.strftime("%H:%M"))
-        
-	time.sleep(20)
+	temp = random.randint(0, 100)
+    	humidity = random.randint(0, 100)
+    	buzzstate = not buzzstate                
+    	GPIO.output(BUZZER,buzzstate)
+    	if(temp>=55):
+		print("temperature have crossed the threshold level on",dt.strftime("%d-%m-%y"),"at",dt.strftime("%H:%M"))
+    	elif(humidity>=78):
+        	print("humidity have crossed the threshold level on",dt.strftime("%d-%m-%y"),"at",dt.strftime("%H:%M"))
+        time.sleep(20)
+	
+   
